5568353b6bb30f20b99f9ebc55e0d954ff773211
BabariyaMit8702 <mit944996@gmail.com>
2025-08-30 14:11:20 +0530

    setup for building apis

--- main/views.py
+++ main/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
+def the_first(request):
+    return HttpResponse('MY APP')
+
+def the_second(request):
+    return HttpResponse('MY API')
